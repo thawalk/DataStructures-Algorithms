5f98c74b4d2a353d977c8cdb8d0b85864df862a9
Tharun Adhi <tharun@resynctech.com>
2020-11-05 17:32:04 +0800

    insertion

---
+++ insertion-sort/insertionSort.py
@@ -0,0 +1,16 @@
+def insertionSort(array):
+    startIdx = 0
+    lastIdx = len(array) - 1    
+    while startIdx != lastIdx:
+        if array[startIdx] > array[startIdx + 1]:
+            for i in reversed(range(startIdx + 2)):
+                if i > 0 and array[i] < array[i - 1]:
+                    swap(i, i - 1, array)
+                else:
+                    break
+        startIdx += 1
+    return array
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+                    
---
+++ selection-sort/selectionSort.py
@@ -0,0 +1,15 @@
+def selectionSort(array):
+    firstIdx = 0
+    lastIdx = len(array) - 1
+    lenOfArray = len(array)
+    while firstIdx != lastIdx:
+        smallestIdx = firstIdx
+        for i in range(firstIdx + 1, lenOfArray):
+            if array[i] < array[smallestIdx]:
+                smallestIdx = i
+        swap(firstIdx, smallestIdx, array)
+        firstIdx += 1
+    return array
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
