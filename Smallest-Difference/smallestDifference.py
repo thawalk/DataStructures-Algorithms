74d743e00971948ca63be9204bc4092f4f40734d
Tharun Adhi <tharun@resynctech.com>
2020-12-24 17:56:22 +0800

    minor-fix

--- Smallest-Difference/smallestDifference.py
+++ Smallest-Difference/smallestDifference.py
@@ -5,21 +5,18 @@ def smallestDifference(arrayOne, arrayTwo):
     quickSort(arrayOne)
     quickSort(arrayTwo)
     lpOne, lpTwo = 0, 0
-    endOne = len(arrayOne) - 1
-    endTwo = len(arrayTwo) - 1
     smallestDiff = abs(arrayOne[lpOne] - arrayTwo[lpTwo])
     smallestPair = [arrayOne[lpOne], arrayTwo[lpTwo]]
-    while lpOne < endOne and lpTwo < endTwo:
-        if smallestDiff == 0:
-            return [arrayOne[lpOne], arrayTwo[lpTwo]]
+    while lpOne < len(arrayOne) - 1 and lpTwo < len(arrayTwo) - 1:
+        if arrayOne[lpOne] < arrayTwo[lpTwo]:
+            lpOne += 1
+        elif arrayOne[lpOne] > arrayTwo[lpTwo]:
+            lpTwo += 1
         else:
-            if arrayOne[lpOne] < arrayTwo[lpTwo]:
-                lpOne += 1
-            else:
-                lpTwo += 1
-            if smallestDiff > abs(arrayOne[lpOne] - arrayTwo[lpTwo]):
-                smallestPair = [arrayOne[lpOne], arrayTwo[lpTwo]]
-                smallestDiff = abs(arrayOne[lpOne] - arrayTwo[lpTwo])
+            return [arrayOne[lpOne], arrayTwo[lpTwo]]
+        if smallestDiff > abs(arrayOne[lpOne] - arrayTwo[lpTwo]):
+            smallestPair = [arrayOne[lpOne], arrayTwo[lpTwo]]
+            smallestDiff = abs(arrayOne[lpOne] - arrayTwo[lpTwo])
     return smallestPair
 
 
