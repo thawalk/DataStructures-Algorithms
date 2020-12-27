55784edac24344fbb82891bc4f7116ffe973ad2b
Tharun Adhi <tharun@resynctech.com>
2020-12-27 17:28:29 +0800

    validateBST

---
+++ Validate-BST/test.py
@@ -0,0 +1,26 @@
+import validateBst
+import unittest
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        root = BST(10)
+        root.left = BST(5)
+        root.left.left = BST(2)
+        root.left.left.left = BST(1)
+        root.left.right = BST(5)
+        root.right = BST(15)
+        root.right.left = BST(13)
+        root.right.left.right = BST(14)
+        root.right.right = BST(22)
+        self.assertEqual(validateBst.validateBst(root), True)
+
+if __name__ == '__main__':
+    unittest.main()
