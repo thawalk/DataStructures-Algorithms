e6ff6952dc950b7cb783b2bcbda942351ebda912
thawalk <tharunana@gmail.com>
2021-01-14 12:38:33 +0800

    group-anagrams-alternative

--- Strings/Group-Anagrams/groupAnagrams.py
+++ Strings/Group-Anagrams/groupAnagrams.py
@@ -1,26 +1,38 @@
 # Time Complexity = O(wnlog(n) + nwlog(w)), where w is the number of words, and n is the length of the longest word | Space Comlexity = O(wn) 
 
-def groupAnagrams(words):
-    if len(words) == 0:
-        return []
+# def groupAnagrams(words):
+#     if len(words) == 0:
+#         return []
 
-    sortedLettersList = ["".join(sorted(word)) for word in words]
-    indexList = [index for index in range(len(words))]
-    indexList.sort(key= lambda x: sortedLettersList[x])
+#     sortedLettersList = ["".join(sorted(word)) for word in words]
+#     indexList = [index for index in range(len(words))]
+#     indexList.sort(key= lambda x: sortedLettersList[x])
     
-    finalResult = []
-    currentGroup = []
-    currentAnagram = sortedLettersList[indexList[0]]
-    for index in indexList:
-        word = words[index]
-        sortedLettersWord = sortedLettersList[index]
+#     finalResult = []
+#     currentGroup = []
+#     currentAnagram = sortedLettersList[indexList[0]]
+#     for index in indexList:
+#         word = words[index]
+#         sortedLettersWord = sortedLettersList[index]
         
-        if sortedLettersWord == currentAnagram:
-            currentGroup.append(word)
-            continue
+#         if sortedLettersWord == currentAnagram:
+#             currentGroup.append(word)
+#             continue
 
-        finalResult.append(currentGroup)
-        currentGroup = [word]
-        currentAnagram = sortedLettersWord
-    finalResult.append(currentGroup)
-    return finalResult
+#         finalResult.append(currentGroup)
+#         currentGroup = [word]
+#         currentAnagram = sortedLettersWord
+#     finalResult.append(currentGroup)
+#     return finalResult
+
+# Time Complexity = O(wnlog(n)), where w is the number of words, and n is the length of the longest word | Space Comlexity = O(wn) 
+
+def groupAnagrams(words):
+    anagrams = {}
+    for word in words:
+        sortedWord = "".join(sorted(word))
+        if sortedWord in anagrams:
+            anagrams[sortedWord].append(word)
+        else:
+            anagrams[sortedWord] = [word]
+    return list(anagrams.values())
