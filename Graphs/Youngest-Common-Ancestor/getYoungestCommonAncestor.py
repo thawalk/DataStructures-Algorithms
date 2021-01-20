ceb263f9e1e3ab6a9d4734fb673c920e4465e87a
thawalk <tharunana@gmail.com>
2021-01-20 11:45:03 +0800

    minor-change

--- Graphs/Youngest-Common-Ancestor/getYoungestCommonAncestor.py
+++ Graphs/Youngest-Common-Ancestor/getYoungestCommonAncestor.py
@@ -6,7 +6,7 @@ class AncestralTree:
 # Time Complexity = O(d) , where d is the depth of the the ancestral tree | Space Complexity = O(1)
 
 def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
-    depthOne, depthTwo = getDepth(descendantOne), getDepth(descendantTwo)
+    depthOne, depthTwo = getDepth(descendantOne, topAncestor), getDepth(descendantTwo, topAncestor)
     if depthOne < depthTwo:
         return helper(descendantOne, descendantTwo, depthTwo - depthOne)
     elif depthTwo < depthOne:
@@ -14,9 +14,9 @@ def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
     else:
         return descendantOne
 
-def getDepth(node):
+def getDepth(node, topAncestor):
     depth = 0
-    while node is not None:
+    while node != topAncestor:
         depth += 1
         node = node.ancestor
     return depth
