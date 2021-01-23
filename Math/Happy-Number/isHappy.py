ca634b52f4e9713bff4ad23246adb3812975deed
thawalk <tharunana@gmail.com>
2021-01-23 15:49:10 +0800

    happyNumber approach 2

--- Math/Happy-Number/isHappy.py
+++ Math/Happy-Number/isHappy.py
@@ -1,3 +1,4 @@
+# <---------------- Approach 1 --------------->
 # Time Complexity = O(log(n)) | Space Complexity = O(1)
 
 def isHappy(n):
@@ -16,3 +17,22 @@ def getNext(n):
         totalSum += (remainder ** 2)
         n = quotient
     return totalSum
+
+# <---------------- Approach 2 --------------->
+# Time Complexity = O(log(n)) | Space Complexity = O(n)
+
+# def isHappy(n):
+#     seen = {}
+#     while n != 1 and n not in seen:
+#         seen[n] = True
+#         n = getNext(n)
+#     return n == 1
+
+
+# def getNext(n):
+#     totalSum = 0
+#     while n > 0:
+#         quotient, remainder = divmod(n, 10)
+#         totalSum += (remainder ** 2)
+#         n = quotient
+#     return totalSum
