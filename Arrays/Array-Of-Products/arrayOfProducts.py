49fdbcbe8238176855650b96080a9cdbbba716db
thawalk <tharunana@gmail.com>
2021-01-26 11:17:24 +0800

    array-of-products-approach-2

--- Arrays/Array-Of-Products/arrayOfProducts.py
+++ Arrays/Array-Of-Products/arrayOfProducts.py
@@ -1,15 +1,40 @@
 # <--------- Approach 1 brute-force ----------->
 # Time Complexity = O(n^2) | Space Complexity = O(1)
 
+# def arrayOfProducts(array):
+#     products = [1 for _ in range(len(array))]
+#     for i in range(len(array)):
+#         runningProduct = 1
+#         for j in range(len(array)):
+#             if i == j:
+#                 continue
+#             runningProduct *= array[j]
+#         products[i] = runningProduct
+#     return products
+
+
+# <--------- Approach 2 using supplementary arrays ----------->
+# Time Complexity = O(n) | Space Complexity = O(n)
+
 def arrayOfProducts(array):
     products = [1 for _ in range(len(array))]
+    leftProducts = [1 for _ in range(len(array))]
+    rightProducts = [1 for _ in range(len(array))]
+    leftProduct, rightProduct = 1, 1
+
+    for i in range(1,len(array)):
+        leftProduct *= array[i-1]
+        leftProducts[i] = leftProduct
+
+    for i in reversed(range(len(array)-1)):
+        rightProduct *= array[i+1]
+        rightProducts[i] = rightProduct
+
     for i in range(len(array)):
-        runningProduct = 1
-        for j in range(len(array)):
-            if i == j:
-                continue
-            runningProduct *= array[j]
-        products[i] = runningProduct
+        products[i] = leftProducts[i] * rightProducts[i]
+    
     return products
 
+
     
+
