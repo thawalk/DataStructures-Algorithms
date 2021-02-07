d6685834dd8c4a7346f18a141b9aaaaa7fef44e7
thawalk <tharunana@gmail.com>
2021-02-07 18:16:58 +0800

    complexities

--- Binary-Trees/Right-Side-View/rightSideView.py
+++ Binary-Trees/Right-Side-View/rightSideView.py
@@ -1,3 +1,5 @@
+# Time Complexity = O(n) | Space Complexity = O(D) where D is a tree diameter
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
