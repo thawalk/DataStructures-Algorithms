f6f2aae4602dc2aeb5f4f200cb88fa19158df7d3
thawalk <tharunana@gmail.com>
2021-02-09 16:42:15 +0800

    iterator

---
+++ Design/Peek-Iterator/peekIterator.py
@@ -0,0 +1,19 @@
+class PeekingIterator:
+    def __init__(self, iterator):
+        self._next = iterator.next()
+        self._iterator = iterator
+
+    def peek(self):
+        return self._next
+
+    def next(self):
+        if self._next is None:
+            raise StopIteration()
+        to_return = self._next
+        self._next = None
+        if self._iterator.hasNext():
+            self._next = self._iterator.next()
+        return to_return
+
+    def hasNext(self):
+        return self._next is not None
