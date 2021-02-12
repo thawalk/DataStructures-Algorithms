ad9b54056dcb09446916bf52065b4b69fcf2ba14
thawalk <tharunana@gmail.com>
2021-02-12 15:40:13 +0800

    better_solution

--- Strings/Valid-Anagram/isAnagram.py
+++ Strings/Valid-Anagram/isAnagram.py
@@ -1,5 +1,23 @@
 # Time Complexity = O(nlogn) | Space Complexity = O(1), is language dependent
 
-def isAnagram(self, s, t):
-    return len(s) == len(t) and sorted(s) == sorted(t)
+# def isAnagram(self, s, t):
+#     return len(s) == len(t) and sorted(s) == sorted(t)
 
+# Time Complexity = O(n) | Space Complexity = O(1), is language dependent
+
+def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        table = [0] * 26
+        
+        for char in s:
+            table[ord(char) - ord('a')] += 1
+            
+        for char in t:
+            table[ord(char) - ord('a')] -= 1
+            
+        for value in table:
+            if value != 0:
+                return False
+        return True
