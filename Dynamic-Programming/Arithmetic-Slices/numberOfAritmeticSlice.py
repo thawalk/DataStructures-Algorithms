4a9c17c1ec1a44b0cdabdd341f3e44763c4953e3
thawalk <tharunana@gmail.com>
2021-02-19 11:42:57 +0800

    arithmetic-slices

---
+++ Dynamic-Programming/Arithmetic-Slices/numberOfAritmeticSlice.py
@@ -0,0 +1,12 @@
+# Time Complexity = O(n) | Space Complexity = O(n)
+
+def numberOfArithmeticSlices(self, A):
+    dp = 0
+    res = 0
+    for i in range(2, len(A)):
+        if A[i] - A[i-1] == A[i - 1] - A[i-2]:
+            dp = 1 + dp
+            res += dp
+        else:
+            dp = 0
+    return res
