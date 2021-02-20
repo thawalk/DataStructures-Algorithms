f9a7e8ddbc1d3cc9c9e5854c740effb063e95b57
thawalk <tharunana@gmail.com>
2021-02-20 20:30:44 +0800

    roman-to-int

---
+++ Strings/Roman-To-Integer/romanToInt.py
@@ -0,0 +1,23 @@
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+# Time Complexity = O(1) | Space Complexity = O(1)
+
+def romanToInt(s):
+    total = 0
+    i = 0
+    while i < len(s):
+        if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+            total += values[s[i + 1]] - values[s[i]]
+            i += 2
+        else:
+            total += values[s[i]]
+            i += 1
+    return total
